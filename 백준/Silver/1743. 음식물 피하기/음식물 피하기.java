95d5eb35179363a7e9e7b24e06e77ec09aafefb7
bagjuna <juna.park124@gmail.com>
2025-05-12 21:58:15 +0900

    [Silver I] Title: 음식물 피하기, Time: 304 ms, Memory: 28784 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
@@ -1,75 +1,74 @@
+
 import java.util.*;
-import java.io.*;
 
 public class Main {
     static boolean[][] map;
     static boolean[][] visited;
-    static int dx[]= {-1,1,0,0};
-	static int dy[]= {0,0,-1,1};
+    static int dx[] = {-1, 1, 0, 0};
+    static int dy[] = {0, 0, -1, 1};
     static int max = Integer.MIN_VALUE;
     static int cnt = 0;
     static int N, M;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         N = sc.nextInt();
         M = sc.nextInt();
         map = new boolean[N][M];
         visited = new boolean[N][M];
         int Num = sc.nextInt();
-        for(int i = 0; i< Num ;i++){
+        for (int i = 0; i < Num; i++) {
             int r = sc.nextInt();
             int c = sc.nextInt();
-            map[r-1][c-1] = true;
+            map[r - 1][c - 1] = true;
         }
-        
-            for(int i = 0; i< N; i++){
-                for(int j = 0; j< M; j++){
-                    if(map[i][j] && !visited[i][j]){
-                    cnt = 0;
-                    bfs(i,j);
-                    max = Math.max(max, cnt);          
-                 }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] && !visited[i][j]) {
+
+                    bfs(i, j);
+                }
             }
-        }        
-    
+        }
+
         System.out.println(max);
     }
-    
-    static void bfs(int i, int j){
-         Queue<Edge> q = new ArrayDeque();
 
-    
-            q.add(new Edge(i,j));
-            visited[i][j] = true;    
-            cnt++;
-            while(!q.isEmpty()) {
-                Edge temp = q.poll();
-                
-                for (int k = 0; k < 4; k++) {
-				int xx = temp.numX +dx[k];
-				int yy = temp.numY +dy[k];
-				if(xx<0 || yy<0 || xx>=N || yy>=M)continue;
-				if(!visited[xx][yy] && map[xx][yy]) {
-					q.add(new Edge(xx, yy));
-					visited[xx][yy]=true;
-					cnt++;
-				}
-    
-			}
+    static void bfs(int i, int j) {
+        cnt = 1;
+        Queue<Edge> q = new ArrayDeque();
 
-         }
-        
+        q.add(new Edge(i, j));
+        visited[i][j] = true;
+        while (!q.isEmpty()) {
+            Edge temp = q.poll();
+            for (int k = 0; k < 4; k++) {
+                int xx = temp.numX + dx[k];
+                int yy = temp.numY + dy[k];
+                if (xx < 0 || yy < 0 || xx >= N || yy >= M) continue;
+                if (!visited[xx][yy] && map[xx][yy]) {
+                    q.add(new Edge(xx, yy));
+                    visited[xx][yy] = true;
+                    cnt++;
+                }
+
+            }
+
+        }
+        max = Math.max(max, cnt);
+
+
     }
-    
-    
-    static class Edge{
+
+
+    static class Edge {
         int numX;
         int numY;
-       
-    
-        Edge(int numX, int numY){
+
+
+        Edge(int numX, int numY) {
             this.numX = numX;
             this.numY = numY;
         }
