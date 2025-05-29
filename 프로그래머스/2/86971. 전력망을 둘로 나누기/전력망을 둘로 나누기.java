8aeeb833f3624ed9a0a42966827110c2bf0fc289
bagjuna <juna.park124@gmail.com>
2025-05-29 22:58:07 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 14.15 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,52 +1,47 @@
 import java.util.*;
 class Solution {
+    static int N;
     public int solution(int n, int[][] wires) {
-        int answer = -1;
+        N = n;
         int minValue = Integer.MAX_VALUE;
-        // 모든 간선 순회
-        for(int i = 0; i< wires.length;i++){
-            HashSet<Integer>[] graph = new HashSet[n+1];
-            // 그 중 하나씩 간선을 없에서 bfs에 돌린다.
-            for(int j = 1; j< n+1;j++){
+        for(int i = 0; i< wires.length; i++){
+            HashSet<Integer>[] graph = new HashSet[n + 1];
+            for(int j = 0; j< n + 1 ;j++){
                 graph[j] = new HashSet<>();
             }
-            for(int j = 0; j < wires.length; j++){
-                int a = wires[j][0];
-                int b = wires[j][1];
-                if(j==i) continue;
-                graph[a].add(b);
-                graph[b].add(a);
+            
+            for(int j =0 ;j <wires.length;j++){
+                if(i==j) continue;
+                graph[wires[j][0]].add(wires[j][1]);
+                graph[wires[j][1]].add(wires[j][0]);
             }
             
-            int count = bfs(graph);
+            
+            
+          int count = bfs(graph);
             int count2 = n - count;
             minValue = Integer.min(minValue, Math.abs(count - count2));
             
         }
-        
-        
         return minValue;
     }
     
-    static int bfs(HashSet<Integer>[] graph){
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(1);
-        HashSet<Integer> visited = new HashSet<>();
-        int cnt = 1;
-        while(!q.isEmpty()){
-            int curr = q.poll();
-          
-            HashSet<Integer>list = graph[curr];
-                for(int next :list){
-                     if(!visited.contains(next)){
-                         visited.add(curr);
-                         q.add(next);
-                         cnt++;
-                     }
+     static int bfs(HashSet<Integer>[] graph){
+            Queue<Integer> q = new LinkedList<>();
+            q.add(1);
+            int cnt = 1;
+            HashSet<Integer> visited = new HashSet<>();
+         
+            while(!q.isEmpty()){
+                int curr = q.poll();
+                
+                for(int next: graph[curr]){
+                    if(visited.contains(next)) continue;
+                    q.add(next);
+                    visited.add(curr);
+                    cnt++;
                 }
-              
-            
+            }
+            return cnt;
         }
-        return cnt;
-    }
 }
