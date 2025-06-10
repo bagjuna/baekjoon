6999f78b7a26a88acf6dc909541b87b4160a60f4
bagjuna <juna.park124@gmail.com>
2025-06-10 10:30:46 +0900

    [Silver III] Title: 퇴사, Time: 172 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,33 +1,49 @@
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) {
         // 코드를 작성해주세요
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[][] work = new int [N][2];
-        
+        List<Work> works = new ArrayList<>();
+
         for(int i = 0; i < N; i++) {
-            work[i][0] = sc.nextInt();
-            work[i][1] = sc.nextInt();
+            Work temp = new Work(sc.nextInt(), sc.nextInt());
+            works.add(temp);
         }
-        
+
         int[] dp = new int[N + 1];
-        
+
         for(int i = 0; i < N; i++) {
-            for(int j = i+work[i][0]; j<N+1; j++ ){
-                if(dp[j] < dp[i] + work[i][1]){
-                    dp[j] = dp[i]+ work[i][1];
+            Work curr = works.get(i);
+            for(int j = i + curr.time; j < N + 1; j++){
+                if(dp[j] < dp[i] + curr.price){
+                    dp[j] = dp[i]+ curr.price;
                 }
             }
-            
+
         }
         System.out.println(dp[dp.length-1]);
-        
-        
+
+
     }
-    
-   
-    
+
+    static class Work{
+        int time;
+        int price;
+        Work(int time, int price){
+            this.time = time;
+            this.price = price;
+        }
+
+        @Override
+        public String toString() {
+            return "Work{" +
+                    "price='" + price + '\'' +
+                    ", time=" + time +
+                    '}';
+        }
+    }
+
+
 }
