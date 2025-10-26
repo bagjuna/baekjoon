0a2d87a256aaf2cd2ccf87b8aa0e84550e932011
bagjuna <juna.park124@gmail.com>
2025-10-26 11:04:12 +0900

    [level 2] Title: 기능개발, Time: 2.97 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,52 +3,26 @@ import java.util.*;
 
 class Solution {
     
-    public int[] solution(int[] progresses, int[] speeds) {
-        int N = progresses.length;
-        int[] answer = new int[N];
+public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> answer = new ArrayList<>();
 
-        for (int i = 0; i < N; i++) {
-            int progres = progresses[i];
-            int time = 0;
-            while (progres < 100) {
-                time++;
-                progres += speeds[i];
+        for (int i = 0; i < progresses.length; i++) {
+            // 1. 한 개의 기능을 개발하는데 필요한 날짜 게산
+            double days = (100 - progresses[i])/ (double) speeds[i];
+            int dayUp = (int) Math.ceil(days);
+
+            // 2. 함께 배포할 기능의 index 찾기
+            int j = i + 1;
+            for(; j < progresses.length; j++){
+                if (progresses[j] + dayUp * speeds[j] < 100) 
+                    break;
+                
             }
-            answer[i] = time;
+            // 3. 이번에 배포할 기능의 개수 추가하기
+            answer.add(j - i);
+            i = j - 1;
         }
-        Queue<Integer> q = new LinkedList<>();
-        List<Integer> list2 = new ArrayList<>();
-        for (int i : answer) {
-            q.add(i);
-        }
-
-        int cnt = 0;
-        while (!q.isEmpty()) {
-            int cnt_temp = 0;
-            int temp = q.peek();
-            if (temp <= cnt) {
-                while (temp <= cnt && !q.isEmpty() && q.peek() <= cnt) {
-                    cnt_temp++;
-                    q.poll();
-                }
-            }
-            if (cnt_temp > 0) {
-                list2.add(cnt_temp);
-            }
-
-            cnt++;
-
-        }
-        
-        
-        int [] result = new int[list2.size()];
-        if (list2.isEmpty()) {
-            return result;
-        }
-        for (int i = 0; i < list2.size(); i++) {
-            result[i] = list2.get(i);
-        }
-        
-        return result;
+        return answer.stream().mapToInt(i -> i.intValue()).toArray();
     }
+    
 }
