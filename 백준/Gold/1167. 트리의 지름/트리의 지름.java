8a386abd47dfc71b21402de481347dba5712d9f9
bagjuna <juna.park124@gmail.com>
2026-01-07 13:41:09 +0900

    [Gold II] Title: 트리의 지름, Time: 692 ms, Memory: 92740 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -5,24 +5,24 @@ public class Main {
 	static int N;
 	static List<Edge>[] graph;
     static boolean[] visited;
-    static int[] answer;
+    static int[] dist;
  	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		graph = new ArrayList[N + 1];
         visited = new boolean[N + 1];
-        answer = new int[N + 1];
+        dist = new int[N + 1];
 		for (int i = 1; i < N + 1; i++) {
 			graph[i] = new ArrayList<>();
 		}
 		for (int i = 1; i < N + 1; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int curr = Integer.parseInt(st.nextToken());
-			int from = 0;
 			int to = 0;
-			while ((from = Integer.parseInt(st.nextToken())) != -1) {
-				to = Integer.parseInt(st.nextToken());
-				graph[curr].add(new Edge(from, to));
+			int value = 0;
+			while ((to = Integer.parseInt(st.nextToken())) != -1) {
+				value = Integer.parseInt(st.nextToken());
+				graph[curr].add(new Edge(to, value));
 			}
 		}
         
@@ -30,21 +30,21 @@ public class Main {
         int max = 0;
         int index = 0;
         for(int i = 2; i< N + 1;i++){
-            if(max < answer[i]){
-                max = answer[i];
+            if(max < dist[i]){
+                max = dist[i];
                 index = i;
             }
         }
         
 
         visited = new boolean[N + 1];
-        answer = new int[N + 1];
+        dist = new int[N + 1];
 
 
         bfs(index);
         
         max = 0;
-        for(int i: answer){
+        for(int i: dist){
             max = Math.max(i, max);
         }
         
@@ -64,7 +64,7 @@ public class Main {
                 if(!visited[to]){
                     visited[to] = true;
                     q.add(to);
-                    answer[to] = answer[curr] + value;
+                    dist[to] = dist[curr] + value;
                 }
                 
             }
