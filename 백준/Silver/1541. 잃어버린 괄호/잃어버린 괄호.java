c8f8fd8b930d36586e027297fa40427d3483b48c
bagjuna <juna.park124@gmail.com>
2026-01-08 13:16:50 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -4,32 +4,28 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         String input = br.readLine();
-
-        int sum = Integer.MAX_VALUE;
-
+        
         String[] split1 = input.split("-");
-
-        int result = 0;
-
-        for (int i = 0; i < split1.length; i++) {
-            int temp = 0;
-
-            String[] split2 = split1[i].split("\\+");
-
-            for (int j = 0; j < split2.length; j++) {
-                temp += Integer.parseInt(split2[j]);
-            }
-
-            if (sum == Integer.MAX_VALUE) {
-                sum = temp;
-            }else {
-                sum -= temp;
-            }
-
+        
+        String[] first = split1[0].split("\\+");
+        
+        int answer = 0;
+        for(int i = 0; i< first.length; i++){
+            answer += Integer.parseInt(first[i]);
         }
-        System.out.println(sum);
+        
+        for(int i = 1; i< split1.length;i++){
+            String line = split1[i];
+            String[] split2 = line.split("\\+");
+            for(int j = 0; j<split2.length;j++){
+                answer -= Integer.parseInt(split2[j]);
+            }
+        }
+        
+        System.out.println(answer);
+        
 
     }
 }
