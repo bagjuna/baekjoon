01558f5d3da518139635c6b9a17d0583b9259cb8
bagjuna <juna.park124@gmail.com>
2026-01-08 12:48:49 +0900

    [Gold V] Title: 회의실 배정, Time: 460 ms, Memory: 43600 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int[][] A = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            A[i][0] = Integer.parseInt(st.nextToken());
+            A[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+
+        int cnt = 0;
+        int end = -1;
+        for (int i = 0; i < N; i++) {
+            if (A[i][0] >= end) {
+                end = A[i][1];
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+
+    }
+}
