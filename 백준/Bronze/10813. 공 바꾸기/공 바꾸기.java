95cf75d8889c67042ad2183205f2732e72d2952c
bagjuna <juna.park124@gmail.com>
2026-02-21 23:53:06 +0900

    [Bronze II] Title: 공 바꾸기, Time: 200 ms, Memory: 18556 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,28 +1,31 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int M = scanner.nextInt();
-
-        int a = 0;
-        int b = 0;
-        int temp = 0;
-
-        int basket[] = new int[N+1];
-        for (int i = 1; i < N + 1; i++) {
-            basket[i] = i;
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] arr = new int[N + 2];
+        for(int i = 1; i < N+1; i++){
+            arr[i] = i;
         }
-        for (int i = 0; i < M; i++) {
-            a = scanner.nextInt();
-            b = scanner.nextInt();
-            temp = basket[a];
-            basket[a]=basket[b];
-            basket[b] = temp;
+        
+        for(int i = 0; i< M;i++){
+            int start = sc.nextInt();
+            int to = sc.nextInt();
+            int temp = arr[start];
+            arr[start] = arr[to];
+            arr[to] = temp;
         }
-        for (int i = 1; i < N + 1; i++) {
-            System.out.print(basket[i]+" ");
+        
+          for(int i = 1; i < N + 1; i++){
+            System.out.print(arr[i] + " ");
         }
+        
     }
+    //  1 2 3 4 5
+    //  2 1 3 4 5
+    //  2 1 4 3 5
+    //  3 1 4 2 5
+    //  3 1 4 2 5
 }
