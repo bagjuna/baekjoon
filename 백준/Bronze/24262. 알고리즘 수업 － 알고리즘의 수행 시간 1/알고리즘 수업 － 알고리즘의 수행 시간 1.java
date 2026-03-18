ffd341b17262bbdf2574603f40ede71d425368de
bagjuna <juna.park124@gmail.com>
2026-03-18 23:53:23 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 176 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 1. 실행 횟수
+        // 1
+        System.out.println(1);
+        
+        // 2, 최고차수
+        // 0
+        System.out.println(0);
+        
+        
+    }
+}
