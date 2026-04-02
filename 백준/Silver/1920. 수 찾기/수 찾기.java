470d54c2088cbe8faa0bea9aa602613866ab2dbf
bagjuna <juna.park124@gmail.com>
2026-04-02 23:45:43 +0900

    [Silver IV] Title: 수 찾기, Time: 1736 ms, Memory: 186688 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,32 +1,39 @@
-import java.io.*;
-import java.util.HashSet;
+
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        HashSet<String> set = new HashSet<>();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] a = new int[N];
+		for (int i = 0; i < N; i++) {
+			a[i] = sc.nextInt();
+		}
+		Arrays.sort(a);
+		int M = sc.nextInt();
 
-        String[] charArray = br.readLine().split(" ");
-        for (String string : charArray) {
-            set.add(string);
-        }
+		for (int i = 0; i < M; i++) {
+			boolean find = false;
+			int target = sc.nextInt();
+			int start = 0;
+			int end = N - 1;
+			while (start <= end) {
+				int midI = (start + end) / 2;
+				int midV = a[midI];
+				if (midV > target) {
+					end = midI - 1;
+				} else if (midV < target) {
+					start = midI + 1;
+				} else {
+					find = true;
+					break;
+				}
+			}
+			if (find)
+				System.out.println(1);
+			else
+				System.out.println(0);
 
-        int M = Integer.parseInt(br.readLine());
-        String[] charArray2 = br.readLine().split(" ");
-
-        for (int i = 0; i < M; i++) {
-            if (set.contains(charArray2[i])) {
-                charArray2[i] = "1";
-            }else {
-                charArray2[i] = "0";
-            }
-        }
-        for (String c : charArray2) {
-            bw.write(c + "\n");
-        }
-        bw.flush();
-        bw.close();
-    }
+		}
+	}
 }
